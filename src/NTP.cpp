8207126dacf01385c7bb0f49520f78574a1afa3f
Ian Lunam <ian.lunam@roboticsplus.co.nz>
2025-01-17 07:57:14 +1300

    Super secret

--- src/NTP.cpp
+++ src/NTP.cpp
@@ -2,14 +2,13 @@
 #include "NTP.h"
 #include <time.h>
 
-const char* tz = "NZST-12NZDT,M9.5.0,M4.1.0/3";
 struct tm timeinfo;
 
 NTP::NTP(){}
 
 void NTP::start()
 {
-    configTzTime(tz, "nz.pool.ntp.org");
+    configTzTime(TIMEZONE, "nz.pool.ntp.org");
     if (!getLocalTime(&timeinfo)) {
         return;
     }
