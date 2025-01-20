b1d12b79b615d9ded39c00fb70a8952a8871d834
Ian Lunam <ian.lunam@roboticsplus.co.nz>
2025-01-21 10:17:22 +1300

    Turn off LED

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,14 @@ void setup() {
     Serial.begin(115200);
     Serial.println("Starting");
 
+    // Turn off LED
+    pinMode(LED_RED, OUTPUT);
+    pinMode(LED_GREEN, OUTPUT);
+    pinMode(LED_BLUE, OUTPUT);
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_GREEN, HIGH);
+    digitalWrite(LED_BLUE, HIGH);
+
     display.init();
     network.connect();
     ldr.start(display);
